8cd255ad5b8cb14189dac663aa0e9df2072ceda1
Pavel <grebeniuk.p@gmail.com>
2020-01-18 10:01:04 +0200

    avg & total

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
-const arrFromSalaryCol = document.querySelectorAll('tr:not(:nth-last-child(-n+2))>td:last-child');
-const arrFromGenderCol = document.querySelectorAll('tr:not(:nth-last-child(-n+2))>td:nth-last-child(2)');
-
-const arrSalaryData = [...arrFromSalaryCol].map(el => +el.innerHTML);
-const arrGenderData = [...arrFromGenderCol].map(el => el.innerHTML);
+const arrSalaryData = [...document.querySelectorAll('tr:not(:nth-last-child(-n+2))>td:last-child')].map(el => +el.innerHTML);
+const arrGenderData = [...document.querySelectorAll('tr:not(:nth-last-child(-n+2))>td:nth-last-child(2)')].map(el => el.innerHTML);
 
 function CreateObj(arr, el, i) {
     this.gender = el;
     this.salary = arr[i];
 }
-const getArrayByGender = (arrSal, arrGend) => arrGend.map((el, i) => new CreateObj(arrSal, el, i));
-const getSalarySum = arr => arr.reduce((sum, el) => sum + el, 0);
-const getGenderSum = (arr, gender) => arr.reduce((acc, el) => el.gender === gender ? acc += el.salary : acc, 0);
+const getArrayByGendSal = (arrSal, arrGend) => arrGend.map((el, i) => new CreateObj(arrSal, el, i));
+const getTotal = (arr, gender) => arr.reduce((acc, el) => !gender ? acc + el.salary : (gender === el.gender ? acc + el.salary : acc), 0);
 const getAvg = (arr, gender) => (arrFilt => arrFilt.reduce((acc, el) => acc + el.salary, 0) / arrFilt.length)(arr.filter(el => !gender ? true : (el.gender === gender ? true : false)));
 
-document.getElementById('total').innerHTML = getSalarySum(arrSalaryData);
-document.getElementById('avg').innerHTML = getAvg(getArrayByGender(arrSalaryData, arrGenderData));
-document.getElementById('totalMale').innerHTML = getGenderSum(getArrayByGender(arrSalaryData, arrGenderData), 'Male');
-document.getElementById('totalFamale').innerHTML = getGenderSum(getArrayByGender(arrSalaryData, arrGenderData), 'Famale');
-document.getElementById('avgMale').innerHTML = Math.round(getAvg(getArrayByGender(arrSalaryData, arrGenderData), 'Male'));
-document.getElementById('avgFamale').innerHTML = Math.round(getAvg(getArrayByGender(arrSalaryData, arrGenderData), 'Famale'));
+const arrOfObj = getArrayByGendSal(arrSalaryData, arrGenderData);
+
+document.getElementById('total').innerHTML = getTotal(arrOfObj);
+document.getElementById('avg').innerHTML = getAvg(arrOfObj);
+document.getElementById('totalMale').innerHTML = getTotal(arrOfObj, 'Male');
+document.getElementById('totalFamale').innerHTML = getTotal(arrOfObj, 'Famale');
+document.getElementById('avgMale').innerHTML = Math.round(getAvg(arrOfObj, 'Male'));
+document.getElementById('avgFamale').innerHTML = Math.round(getAvg(arrOfObj, 'Famale'));
